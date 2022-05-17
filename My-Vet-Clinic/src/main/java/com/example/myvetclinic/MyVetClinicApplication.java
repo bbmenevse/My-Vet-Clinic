5a5687117a05940e5bcf5604c572db14a91600db
bbmenevse <bbmenevse@gmail.com>
2022-05-17 13:17:37 +0300

    Initial Project Add

---
+++ My-Vet-Clinic/src/main/java/com/example/myvetclinic/MyVetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.myvetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVetClinicApplication.class, args);
+	}
+
+}
---
+++ My-Vet-Clinic/src/test/java/com/example/myvetclinic/MyVetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myvetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
