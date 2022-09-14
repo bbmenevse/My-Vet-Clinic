278ed5fad3173bc37b6ef06476ce03af87fa12d9
bbmenevse <bbmenevse@gmail.com>
2022-09-14 16:07:58 +0300

    Transfered contents of src and then deleted it. Checking how to close issue. closes #1

--- pet-clinic-web/src/main/java/com/example/myvetclinic/MyVetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/example/myvetclinic/MyVetClinicApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyVetClinicApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyVetClinicApplication.class, args);
 	}
--- pet-clinic-web/src/test/java/myvetclinic/MyVetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/myvetclinic/MyVetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.myvetclinic;
+package myvetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
