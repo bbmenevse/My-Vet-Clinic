c4f53800f3571a8f7d5667ed7bac7838a7dbfc15
bbmenevse <bbmenevse@gmail.com>
2022-10-20 15:15:36 +0300

    OwnerServiceMap

---
+++ pet-clinic-data/src/main/java/com/example/myvetclinic/services/map/OwnerSericeMap.java
@@ -0,0 +1,34 @@
+package com.example.myvetclinic.services.map;
+
+import com.example.myvetclinic.model.Owner;
+import com.example.myvetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerSericeMap extends AbstractMapService<Owner,Long> implements CrudService<Owner,Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
