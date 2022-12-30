5e3b27a1b847e55c1447757a25d8410654d82482
bbmenevse <bbmenevse@gmail.com>
2022-12-30 12:48:36 +0300

    Made Vet and Speciality classes Entity.

--- pet-clinic-data/src/main/java/com/example/myvetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/example/myvetclinic/model/Speciality.java
@@ -1,7 +1,13 @@
 package com.example.myvetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+
+@Entity
 public class Speciality extends BaseEntity{
 
+    @Column
     private String description;
 
     public String getDescription()
