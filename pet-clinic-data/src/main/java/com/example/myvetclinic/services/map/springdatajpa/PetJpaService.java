e7d32d63a9f6f1680fb29d50d20a8e9e1d4e3927
bbmenevse <bbmenevse@gmail.com>
2023-01-04 23:07:47 +0300

    added Service and profile to PetJpaService and SpecialityJpaService classes.

--- pet-clinic-data/src/main/java/com/example/myvetclinic/services/map/springdatajpa/PetJpaService.java
+++ pet-clinic-data/src/main/java/com/example/myvetclinic/services/map/springdatajpa/PetJpaService.java
@@ -3,7 +3,14 @@ package com.example.myvetclinic.services.map.springdatajpa;
 import com.example.myvetclinic.model.Pet;
 import com.example.myvetclinic.repositories.PetRepository;
 import com.example.myvetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+@Service
+@Profile("springdatajpa")
+@Transactional
 public class PetJpaService extends AbstractJpaService<Pet, PetRepository> implements PetService {
 
     public PetJpaService(PetRepository repository) {
--- pet-clinic-data/src/main/java/com/example/myvetclinic/services/map/springdatajpa/SpecialityJpaService.java
+++ pet-clinic-data/src/main/java/com/example/myvetclinic/services/map/springdatajpa/SpecialityJpaService.java
@@ -3,7 +3,14 @@ package com.example.myvetclinic.services.map.springdatajpa;
 import com.example.myvetclinic.model.Speciality;
 import com.example.myvetclinic.repositories.SpecialityRepository;
 import com.example.myvetclinic.services.SpecialitiesService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+@Service
+@Profile("springdatajpa")
+@Transactional
 public class SpecialityJpaService extends AbstractJpaService<Speciality, SpecialityRepository> implements SpecialitiesService {
 
     public SpecialityJpaService(SpecialityRepository repository) {
